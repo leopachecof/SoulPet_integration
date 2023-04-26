bc5c0198b789e545f233d16b17b46b7605d9fe26
leopachecof <l.nikiti@gmail.com>
2023-04-26 16:30:10 -0300

    Inclusão de cadastro e exclusão de clientes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Root } from "./pages/Root/Root";
 import { Home } from "./pages/Home/Home";
 import { NovoCliente } from "./pages/NovoCliente/NovoCliente";
+import { Clientes } from "./pages/Clientes/Clientes";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Root />}>
           <Route path="/" element={<Home />} />
           <Route path="/clientes/novo" element={<NovoCliente />} />
+          <Route path="/clientes" element={<Clientes />} />
         </Route>
         
       </Routes>
